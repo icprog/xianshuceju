d6a1ea22441928915e4b62d6c5d830c7bdde5b02
r00kies <36098105+r00kies@users.noreply.github.com>
2018-02-05 14:00:52 +0800

    add ANDT

---
+++ ANDT/ANO_DT.c
@@ -0,0 +1,250 @@
+/******************** (C) COPYRIGHT 2014 ANO Tech ********************************
+  * 作者   ：匿名科创
+ * 文件名  ：data_transfer.c
+ * 描述    ：数据传输
+ * 官网    ：www.anotc.com
+ * 淘宝    ：anotc.taobao.com
+ * 技术Q群 ：190169595
+**********************************************************************************/
+
+#include "includes.h"
+
+/////////////////////////////////////////////////////////////////////////////////////
+//数据拆分宏定义，在发送大于1字节的数据类型时，比如int16、float等，需要把数据拆分成单独字节进行发送
+#define BYTE0(dwTemp)       ( *( (char *)(&dwTemp)		) )
+#define BYTE1(dwTemp)       ( *( (char *)(&dwTemp) + 1) )
+#define BYTE2(dwTemp)       ( *( (char *)(&dwTemp) + 2) )
+#define BYTE3(dwTemp)       ( *( (char *)(&dwTemp) + 3) )
+
+dt_flag_t f;					//需要发送数据的标志
+u8 data_to_send[50];	//发送数据缓存
+
+/////////////////////////////////////////////////////////////////////////////////////
+//Data_Exchange函数处理各种数据发送请求，比如想实现每5ms发送一次传感器数据至上位机，即在此函数内实现
+//此函数应由用户每1ms调用一次
+void ANO_DT_Data_Exchange(void)
+{
+	static u8 cnt = 0;
+	static u8 senser_cnt 	= 10;
+	static u8 status_cnt 	= 15;
+	if((cnt % senser_cnt) == (senser_cnt-1))
+		f.send_senser = 1;	
+	
+	if((cnt % status_cnt) == (status_cnt-1))
+		f.send_status = 1;	
+	
+	if((cnt % rcdata_cnt) == (rcdata_cnt-1))
+		f.send_rcdata = 1;	
+	
+	if((cnt % motopwm_cnt) == (motopwm_cnt-1))
+		f.send_motopwm = 1;	
+	
+	if((cnt % power_cnt) == (power_cnt-1))
+		f.send_power = 1;		
+	
+	cnt++;
+/////////////////////////////////////////////////////////////////////////////////////
+	if(f.send_version)
+	{
+		f.send_version = 0;
+		ANO_DT_Send_Version(4,300,100,400,0);
+	}
+/////////////////////////////////////////////////////////////////////////////////////
+	else if(f.send_status)
+	{
+		f.send_status = 0;
+		ANO_DT_Send_Status(Roll,Pitch,Yaw,baroAlt,0,fly_ready);
+	}	
+/////////////////////////////////////////////////////////////////////////////////////
+	else if(f.send_senser)
+	{
+		f.send_senser = 0;
+		ANO_DT_Send_Senser(0,0,0,
+												0,0,0,
+												0,0,0,0);
+	}	
+/////////////////////////////////////////////////////////////////////////////////////
+
+	}
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////			
+/////////////////////////////////////////////////////////////////////////////////////
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//Send_Data函数是协议中所有发送数据功能使用到的发送函数
+//移植时，用户应根据自身应用的情况，根据使用的通信方式，实现此函数
+//void ANO_DT_Send_Data(u8 *dataToSend , u8 length)
+//{
+//#ifdef ANO_DT_USE_USB_HID
+//	Usb_Hid_Adddata(data_to_send,length);
+//#endif
+//#ifdef ANO_DT_USE_USART2
+//	Usart2_Send(data_to_send, length);
+//#endif
+//}
+
+static void ANO_DT_Send_Check(u8 head, u8 check_sum)
+{
+	data_to_send[0]=0xAA;
+	data_to_send[1]=0xAA;
+	data_to_send[2]=0xEF;
+	data_to_send[3]=2;
+	data_to_send[4]=head;
+	data_to_send[5]=check_sum;
+	
+	
+	u8 sum = 0;
+	for(u8 i=0;i<6;i++)
+		sum += data_to_send[i];
+	data_to_send[6]=sum;
+
+	ANO_DT_Send_Data(data_to_send, 7);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+//Data_Receive_Prepare函数是协议预解析，根据协议的格式，将收到的数据进行一次格式性解析，格式正确的话再进行数据解析
+//移植时，此函数应由用户根据自身使用的通信方式自行调用，比如串口每收到一字节数据，则调用此函数一次
+//此函数解析出符合格式的数据帧后，会自行调用数据解析函数
+void ANO_DT_Data_Receive_Prepare(u8 data)
+{
+	static u8 RxBuffer[50];
+	static u8 _data_len = 0,_data_cnt = 0;
+	static u8 state = 0;
+	
+	if(state==0&&data==0xAA)
+	{
+		state=1;
+		RxBuffer[0]=data;
+	}
+	else if(state==1&&data==0xAF)
+	{
+		state=2;
+		RxBuffer[1]=data;
+	}
+	else if(state==2&&data<0XF1)
+	{
+		state=3;
+		RxBuffer[2]=data;
+	}
+	else if(state==3&&data<50)
+	{
+		state = 4;
+		RxBuffer[3]=data;
+		_data_len = data;
+		_data_cnt = 0;
+	}
+	else if(state==4&&_data_len>0)
+	{
+		_data_len--;
+		RxBuffer[4+_data_cnt++]=data;
+		if(_data_len==0)
+			state = 5;
+	}
+	else if(state==5)
+	{
+		state = 0;
+		RxBuffer[4+_data_cnt]=data;
+		ANO_DT_Data_Receive_Anl(RxBuffer,_data_cnt+5);
+	}
+	else
+		state = 0;
+}
+/////////////////////////////////////////////////////////////////////////////////////
+//Data_Receive_Anl函数是协议数据解析函数，函数参数是符合协议格式的一个数据帧，该函数会首先对协议数据进行校验
+//校验通过后对数据进行解析，实现相应功能
+//此函数可以不用用户自行调用，由函数Data_Receive_Prepare自动调用
+
+
+void ANO_DT_Send_Status(float angle_rol, float angle_pit, float angle_yaw, s32 alt, u8 fly_model, u8 armed)
+{
+	u8 _cnt=0;
+	vs16 _temp;
+	vs32 _temp2 = alt;
+	
+	data_to_send[_cnt++]=0xAA;
+	data_to_send[_cnt++]=0xAA;
+	data_to_send[_cnt++]=0x01;
+	data_to_send[_cnt++]=0;
+	
+	_temp = (int)(angle_rol*100);
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = (int)(angle_pit*100);
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = (int)(angle_yaw*100);
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	
+	data_to_send[_cnt++]=BYTE3(_temp2);
+	data_to_send[_cnt++]=BYTE2(_temp2);
+	data_to_send[_cnt++]=BYTE1(_temp2);
+	data_to_send[_cnt++]=BYTE0(_temp2);
+	
+	data_to_send[_cnt++] = fly_model;
+	
+	data_to_send[_cnt++] = armed;
+	
+	data_to_send[3] = _cnt-4;
+	
+	u8 sum = 0;
+	for(u8 i=0;i<_cnt;i++)
+		sum += data_to_send[i];
+	data_to_send[_cnt++]=sum;
+	
+	ANO_DT_Send_Data(data_to_send, _cnt);
+}
+void ANO_DT_Send_Senser(s16 a_x,s16 a_y,s16 a_z,s16 g_x,s16 g_y,s16 g_z,s16 m_x,s16 m_y,s16 m_z,s32 bar)
+{
+	u8 _cnt=0;
+	vs16 _temp;
+	
+	data_to_send[_cnt++]=0xAA;
+	data_to_send[_cnt++]=0xAA;
+	data_to_send[_cnt++]=0x02;
+	data_to_send[_cnt++]=0;
+	
+	_temp = a_x;
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = a_y;
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = a_z;	
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	
+	_temp = g_x;	
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = g_y;	
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = g_z;	
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	
+	_temp = m_x;	
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = m_y;	
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	_temp = m_z;	
+	data_to_send[_cnt++]=BYTE1(_temp);
+	data_to_send[_cnt++]=BYTE0(_temp);
+	
+	data_to_send[3] = _cnt-4;
+	
+	u8 sum = 0;
+	for(u8 i=0;i<_cnt;i++)
+		sum += data_to_send[i];
+	data_to_send[_cnt++] = sum;
+	
+	ANO_DT_Send_Data(data_to_send, _cnt);
+}
+
+/******************* (C) COPYRIGHT 2014 ANO TECH *****END OF FILE************/
